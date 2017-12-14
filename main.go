7ccdba8ad71e8ba8d216a39ad8f1c28eb440a0d9
Tyler Davis <tydavis@gmail.com>
2017-12-14 03:28:02 +0000

    Basic searchreplace

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	var search string
+	var replace string
+	var filename string
+	//var backup bool
+
+	//flag.BoolVar(&backup, "b", "false", "Backup original file with .bak extension")
+	flag.StringVar(&search, "s", "", "Search regex or string")
+	flag.StringVar(&replace, "r", "", "Replace string")
+	flag.StringVar(&filename, "f", "", "Filename to replace")
+	flag.Parse()
+
+	if filename == "" {
+		fmt.Printf("Filename not specified \n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// File must fit in available ram
+	input, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("%v \n", err)
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	lines := strings.Split(string(input), "\n")
+
+	for i, l := range lines {
+		//
+		if strings.Contains(l, search) {
+			rline := strings.Replace(l, search, replace, -1)
+			fmt.Println(rline) //DEBUG
+			lines[i] = rline
+		}
+	}
+
+	output := strings.Join(lines, "\n")
+	err = ioutil.WriteFile(filename, []byte(output), 0644)
+	if err != nil {
+		fmt.Printf("%v \n", err)
+		flag.Usage()
+		os.Exit(1)
+	}
+
+}
